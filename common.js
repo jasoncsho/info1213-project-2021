b3ceeb1fc0d9cc74fe1095194d07359f411fafd3
jasoncsho <87612913+jasoncsho@users.noreply.github.com>
2021-08-02 18:52:41 -0700

    Updated info on About Us
    I have given us avatars based on games on anime
    I am Mario, Harsh is Spike, Taran is Kirito, and haven't heard from Akshat so I employ him or her as comic relief and gave her a female idol named Ichigo from Aikatsu

--- common.js
+++ common.js
@@ -1,3 +1,36 @@
 function redirectToInventory() {
+  const email = document.getElementById("username").value;
+  validemail(email);
   location.href = 'home.html';
 }
+
+ function validemail(email) {
+    var emailvalue=email;
+
+    var validationnum=/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+    if(validationnum.test(validemail) == false)
+
+    {
+
+        alert("This email is invalid try again!");
+
+        return false;
+
+    }
+
+    else
+
+    {
+
+        alert("This is valid");
+
+        return true;
+
+    }        
+
+}
+
+// function redirectToInventory() {
+//   location.href = 'home.html';
+// }
