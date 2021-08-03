9bc7bc7059b93df2c047d8fd25b7d1a89fa6ef9d
jasoncsho <87612913+jasoncsho@users.noreply.github.com>
2021-08-02 19:54:02 -0700

    Added information
    Updated history, home and mission pages. Expect some Lorem Ipsum.

--- common.js
+++ common.js
@@ -1,34 +1,16 @@
 function redirectToInventory() {
   const email = document.getElementById("username").value;
-  validemail(email);
-  location.href = 'home.html';
+  if ( isValidEmail(email) ) {
+    location.href = 'home.html';
+  } else {
+    alert("Your email is invalid. Try again.")
+  }
+  
 }
 
- function validemail(email) {
-    var emailvalue=email;
-
-    var validationnum=/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-
-    if(validationnum.test(validemail) == false)
-
-    {
-
-        alert("This email is invalid try again!");
-
-        return false;
-
-    }
-
-    else
-
-    {
-
-        alert("This is valid");
-
-        return true;
-
-    }        
-
+ function isValidEmail(email) {
+    var validationnum=/\S+@\S+\.\S+/;
+    return validationnum.test(email) == true;
 }
 
 // function redirectToInventory() {
